54a2001c40273f3db07a7bcb4547139dbf491e43
EC2 Default User <ec2-user@ip-10-0-0-36.ec2.internal>
2014-02-04 11:15:25 +0000

    Modded

--- crontab_cookbook/recipes/crontabentry.rb
+++ crontab_cookbook/recipes/crontabentry.rb
@@ -1,8 +1,8 @@
-cron "bundle_command" do
-  minute "*/3"
-  command "/bin/bash -l -c 'cd / && RAILS_ENV=staging /usr/local/bin/bundle exec rake every_three_minute_tasks --silent >> /var/log/whenever.log 2>&1"
-end
-
 execute "prepend_path" do
   command "export PATH=/usr/local/bin:$PATH"
 end
+cron "bundle_command" do
+  minute "*/3"
+  command "/bin/bash -l -c 'which ruby >> /var/log/whenever.log 2>&1 && which bundle >> /var/log/whenever.log"
+end
+
