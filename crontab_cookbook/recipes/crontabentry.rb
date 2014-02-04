c971fbabbc185dfe75f30a56dc4181d76e6a2684
EC2 Default User <ec2-user@ip-10-0-0-36.ec2.internal>
2014-02-04 10:02:27 +0000

    Modded

--- crontab_cookbook/recipes/crontabentry.rb
+++ crontab_cookbook/recipes/crontabentry.rb
@@ -1,5 +1,4 @@
 cron "bundle_command" do
   minute "*/3"
-  hour "*"
   command "/bin/bash -l -c 'cd / && RAILS_ENV=staging /usr/local/bin/bundle exec rake every_three_minute_tasks --silent >> /var/log/whenever.log 2>&1"
 end
