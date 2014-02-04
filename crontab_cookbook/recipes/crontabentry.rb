94677ebe3549062f764377537e846f25b01b083e
EC2 Default User <ec2-user@ip-10-0-0-36.ec2.internal>
2014-02-04 08:40:29 +0000

    Modded

--- crontab_cookbook/recipes/crontabentry.rb
+++ crontab_cookbook/recipes/crontabentry.rb
@@ -1,8 +1,5 @@
 cron "bundle_command" do
-  minute "*/3"
+  minute "1"
   hour "*"
-  command "cd /srv/opscode-community-site/current &&
-    env RUBYLIB='/srv/opscode-community-site/current/lib'
-    RAILS_ASSET_ID=`git rev-parse HEAD`
-    bundle exec rake _report"
+  command "cd /srv/opscode-community-site/current && env RUBYLIB='/srv/opscode-community-site/current/lib' RAILS_ASSET_ID=`git rev-parse HEAD` bundle exec rake _report"
 end
