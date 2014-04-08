7d8c7c5243a860e56aa9f31a53df8cde3718c765
Mark <mark@media.(none)>
2014-04-08 21:44:15 +0200

    Fixed copy.rb's group

--- copy_file/recipes/copy.rb
+++ copy_file/recipes/copy.rb
@@ -2,6 +2,6 @@ Chef::Log.info("Retrieving file.txt")
 remote_file "/tmp/file.txt" do
   source "https://s3.amazonaws.com/tomzynsk/file.txt"
   owner "opsnode_root"
-  group "opsnode_root"
+  group "root"
   mode 0777
 end
