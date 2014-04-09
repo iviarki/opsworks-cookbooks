b5137a6039d18f265a0afca5b20d34dca45d9567
root <root@ip-10-0-0-30.ec2.internal>
2014-04-09 08:33:43 +0000

    Tweaks

--- copy_file/recipes/copy.rb
+++ copy_file/recipes/copy.rb
@@ -1,4 +1,4 @@
-Chef::Log.info("Retrieving file.txt")
+Chef::Log.info("Custom Log Entry: Retrieving file.txt")
 remote_file "/tmp/file.txt" do
   source "https://s3.amazonaws.com/tomzynsk/file.txt"
   owner "opsnode_root"
--- crontab/recipes/crontabentry.rb
+++ crontab/recipes/crontabentry.rb
@@ -1,6 +1,6 @@
-Chef::Log.info("Adding random_cron_job to Crontab")
+Chef::Log.info("Custom Log Entry: Adding random_cron_job to Crontab")
 cron "random_cron_job" do
   minute "*/10"
-  command "echo 'Count/n' >> /tmp/count.log"
+  command "echo 'Count' >> /tmp/count.log"
 end
 
