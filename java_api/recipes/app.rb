c967048415d4f934ec54e5f9f67d1ebe03f3b5a0
iviarki <iviarki@gmail.com>
2014-07-23 08:52:55 +0000

    Blob

--- java_api/recipes/app.rb
+++ java_api/recipes/app.rb
@@ -1,5 +1,5 @@
 application = node[:opsworks][:applications][0][:slug_name]
-user = node[:opsworks][:deploy_user]
+user = node[:opsworks][:deploy_user][:user]
 appdir = node[:deploy][application][:deploy_to]
 
 Chef::Log.info "Deploying #{application} using ownlocal_deploy::default"
